bba5e75d565e5cf4aba465e994ec9b3944bd6622
Lewis lee <lewlee@student.42kl.edu.my>
2023-08-29 12:56:00 +0800

    LL fixed collision

--- srcs/movement/movement.c
+++ srcs/movement/movement.c
@@ -2,32 +2,32 @@
 
 void	up(t_data *data)
 {
-	if(data->map[(int)data->player.posY][(int)(data->player.posX + data->ray.dirX * MOVESPEED * COLLISION_MOD)] == '0')
+	if(ft_strchr("0NSEW", data->map[(int)data->player.posY][(int)(data->player.posX + data->ray.dirX * MOVESPEED * COLLISION_MOD)]))
 		data->player.posX += data->ray.dirX * MOVESPEED;
-    if(data->map[(int)(data->player.posY + data->ray.dirY * MOVESPEED * COLLISION_MOD)][(int)data->player.posX] == '0') 
+    if(ft_strchr("0NSEW", data->map[(int)(data->player.posY + data->ray.dirY * MOVESPEED * COLLISION_MOD)][(int)data->player.posX])) 
 		data->player.posY += data->ray.dirY * MOVESPEED;
 }
 
 void	down(t_data *data)
 {
-	if(data->map[(int)data->player.posY][(int)(data->player.posX - data->ray.dirX * MOVESPEED * COLLISION_MOD)] == '0')
+	if(ft_strchr("0NSEW", data->map[(int)data->player.posY][(int)(data->player.posX - data->ray.dirX * MOVESPEED * COLLISION_MOD)]))
 		data->player.posX -= data->ray.dirX * MOVESPEED;
-    if(data->map[(int)(data->player.posY - data->ray.dirY * MOVESPEED * COLLISION_MOD)][(int)data->player.posX] == '0') 
+    if(ft_strchr("0NSEW", data->map[(int)(data->player.posY - data->ray.dirY * MOVESPEED * COLLISION_MOD)][(int)data->player.posX])) 
 		data->player.posY -= data->ray.dirY * MOVESPEED;
 }
 
 void	left(t_data *data)
 {
-	if(data->map[(int)data->player.posY][(int)(data->player.posX + data->ray.dirY * MOVESPEED * COLLISION_MOD)] == '0')
+	if(ft_strchr("0NSEW", data->map[(int)data->player.posY][(int)(data->player.posX + data->ray.dirY * MOVESPEED * COLLISION_MOD)]))
 		data->player.posX += data->ray.dirY * MOVESPEED;
-    if(data->map[(int)(data->player.posY - data->ray.dirX * MOVESPEED * COLLISION_MOD)][(int)data->player.posX] == '0') 
+    if(ft_strchr("0NSEW", data->map[(int)(data->player.posY - data->ray.dirX * MOVESPEED * COLLISION_MOD)][(int)data->player.posX])) 
 		data->player.posY -= data->ray.dirX * MOVESPEED;
 }
 
 void	right(t_data *data)
 {
-	if(data->map[(int)data->player.posY][(int)(data->player.posX - data->ray.dirY * MOVESPEED * COLLISION_MOD)] == '0')
+	if(ft_strchr("0NSEW", data->map[(int)data->player.posY][(int)(data->player.posX - data->ray.dirY * MOVESPEED * COLLISION_MOD)]))
 		data->player.posX -= data->ray.dirY * MOVESPEED;
-    if(data->map[(int)(data->player.posY + data->ray.dirX * MOVESPEED * COLLISION_MOD)][(int)data->player.posX] == '0') 
+    if(ft_strchr("0NSEW", data->map[(int)(data->player.posY + data->ray.dirX * MOVESPEED * COLLISION_MOD)][(int)data->player.posX])) 
 		data->player.posY += data->ray.dirX * MOVESPEED;
 }
