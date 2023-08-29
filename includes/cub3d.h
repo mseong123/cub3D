2c3be2ff5fb75a110bea1c3fc549819475636bbd
Meng Seong Lee <melee@u82z01s05.42kl.edu.my>
2023-08-29 16:40:21 +0800

    done checking

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -12,8 +12,8 @@
 
 #ifndef CUB3D_H
 # define CUB3D_H
-# define W_WIDTH 1920
-# define W_HEIGHT 1080
+# define W_WIDTH 1280
+# define W_HEIGHT 640
 # define KEYPRESS 2
 # define KEYRELEASE 3
 # define DESTROY 17
